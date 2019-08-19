70d2a9c4f601f8b5ac73136456aebee983faf6a8
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-19 21:58:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,11 @@
 def starts_with_a_vowel?(word)
-  
-end
+  if word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
+ end
+
 
 def words_starting_with_un_and_ending_with_ing(text)
 #returns an array with the words starting with 'un' and ending with 'ing'
